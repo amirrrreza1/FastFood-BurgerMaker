fdcd44cd2b75df83e1f8db86c4f2c022f63fa0e9
Amirreza <arazarioun83@gmail.com>
2025-06-29 01:59:48 +0330

    feat: add web app manifest and update metadata for improved PWA support

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "FastFood Restaurant",
+    short_name: "AmirFast",
+    description:
+      "A fast food restaurant offering online food ordering with the best quality and fastest delivery.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#a58a01",
+    icons: [
+      {
+        src: "/Logo192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/Logo512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
